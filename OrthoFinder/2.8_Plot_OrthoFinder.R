d2680f4a92a4a4a1de5ca0086407962e92bef4be
mjb51923 <58435529+mjb51923@users.noreply.github.com>
2023-10-05 11:32:40 -0400

    Update 2.8_Plot_OrthoFinder.R

--- OrthoFinder/2.8_Plot_OrthoFinder.R
+++ OrthoFinder/2.8_Plot_OrthoFinder.R
@@ -4,6 +4,7 @@ library(splitstackshape)
 library(karyoploteR)
 library(readxl)
 library(ggplot2)
+
 library(pafr)
 library(Rsamtools)
 
@@ -104,14 +105,13 @@ Other_end$Ab10_seqname <- "Ab10"
 Other_start_range <- toGRanges(Other_start)
 Other_end_range <- toGRanges(Other_end)
 
-#These numbers refer to the last 3 digits in the B73-N10 v5 gene names
-#gene 490 is at 151806944 in B73 
-#gene 500 is at 151809681 in B73 
-#gene 510 is at 151941902 in B73 
-#gene 520 is at 151944890 in B73 
-#gene 530 is at 151946938 in B73 
-#gene 540 is at 151948302 in B73 
-#I removed the genes (510 and 520) that have an orthologe in the inversion so that I only highlight the orthologes that could be sr2
+#my 490 gene is at 151806944 in B73 
+#gene 500 is at 151809681
+#gene 510 is at 151941902
+#gene 520 is at 151944890
+#gene 530 is at 151946938
+#gene 540 is at 151948302
+#I removed the gene (510 and 520) that have an inverted orthologue so that I only highlight the orthologues that could contain sr2
 Dup <- subset(OrthoLink_10_temp1, B73_start == 151806944 | B73_start == 151809681 | B73_start == 151946938 | B73_start == 151948302)
 Dup_start <- Dup[,c("B73_seqname", "B73_start", "B73_end")]
 Dup_end <- Dup[,c("Ab10_seqname", "Ab10_start", "Ab10_end")]
@@ -120,19 +120,21 @@ Dup_end$Ab10_seqname <- "Ab10"
 Dup_start_range <- toGRanges(Dup_start)
 Dup_end_range <- toGRanges(Dup_end)
 
-#This section loads in the position of known morphological markers on B73-N10 and Ab10 and the position of the Df() line breakpoints on Ab10 
+
 #B73
-#W2 is at 146758676 in B73-N10
-#O7 is at 150834905 in B73-N10
+#W2 is at 146758676
+#O7 is at 150834905
+#Sr2 is at 151806944
 x_N10 <- c(146758676, 150834905)
 y_N10 <- c(0.2, 0.2)
-genes_N10 <- c("W2", "O7")
+genes_N10 <- c("o7", "w2")
 
-#W2 is 167105020 in B73-Ab10
-#O7 is 161788577 in B73-Ab10
+#W2 is 167105020
+#O7 is 161788577
+#Sr2 is at 167968911
 x_Ab10 <- c(161788577, 167105020, 167426118, 172190001)
 y_Ab10 <- c(-0.45, -0.45, -.6, -.6)
-genes_Ab10 <- c("O7", "W2", "Df(K)", "DF(M)")
+genes_Ab10 <- c("o7", "w2", "Df(K)", "DF(M)")
 
 #This alters plot parameters, this is actually just the default still
 pp <- getDefaultPlotParams(plot.type=1)
@@ -153,7 +155,7 @@ B73_GFF_Heat_filt$seqname <- "N10"
 B73_GFF_Heat_filt$y <- 0.1
 B73_GFF_range <- toGRanges(B73_GFF_Heat_filt)
 
-#This reads in the bam file with the B73_N10 short read data. This file was generated in step 1.3
+#This reads in the bam file 
 BED <- read.delim("/Volumes/Transcend/BWAmem_B73.Ab10MAPQ20Prim.s.bam.bedgraph", header=FALSE)
 BED$V4 <- BED$V4/100
 xs=BED$V2
@@ -162,15 +164,12 @@ yb <- rep(0,nrow(BED))
 yt=BED$V4
 
 
-###### In this section I pull out all the Gene Names for Ab10 and B73 with unexpected orthologes 
+###### In this section I want to pull out all the Gene Names for Ab10 and B73 with unexpected orthologues 
+
 Unexpected_temp <- subset(OrthoLink_10_temp1, OrthoLink_10_temp1$Ab10_start >= 167968902 | (OrthoLink_10_temp1$Ab10_start > 143960000 & OrthoLink_10_temp1$Ab10_start < 150960000))
+
 Unexpected <- subset(Unexpected_temp, Unexpected_temp$B73_start >= 140960000)
 Unexpected
-
-#I manually got the GO terms from maize GDB
-GO <- read_excel("~/University_of_Georgia/Dawe_Lab_Documents/Striated/Unexpected_Orthologues_Between_N10SharedRegion_Ab10.xlsx")
-Unexpected_GO <- merge(Unexpected, GO)
-write.csv(Unexpected_GO, file="Unexpected_Orthologues_Between_N10SharedRegion_Ab10_Positions.csv", row.names = FALSE)
 
 #This does the plotting
 png(file="Orthologes_KaryoploteR.png", width = 480*4, height = 480*3, units = "px", pointsize = 24, bg = "white")
@@ -185,8 +184,6 @@ kpRect(kp, chr="Ab10", x0=174433450, x1=182846100, y0=-0.25, y1=-0.35, col = "gr
 kpRect(kp, chr="Ab10", x0=148964528, x1=149082763, y0=-0.25, y1=-0.35, col = "blue", border= "blue")
 kpRect(kp, chr="Ab10", x0=189326066, x1=190330226, y0=-0.25, y1=-0.35, col = "darkgreen")
 kpRect(kp, chr="Ab10", x0=167426118, x1=172190001, y0=-0.54, y1=-0.54, col = "black")
-kpAddLabels(kp, labels="Features", r0=-.3, r1 =-.3)
-kpAddLabels(kp, labels="Df() Breakpoints", r0=-.5, r1 =-.5)
 kpPlotLinks(kp, data=Uninverted_start_range, data2=Uninverted_end_range, r0=-.5, r1 = -.25, y= 1.6, col = "palegreen", border = "palegreen" )
 kpPlotLinks(kp, data=Inv1_start_range, data2=Inv1_end_range, r0=-.5, r1 = -.25, y= 1.6, col = "seagreen1", border = "seagreen1")
 kpPlotLinks(kp, data=Inv2_start_range, data2=Inv2_end_range, r0=-.5, r1 = -.25, y= 1.6, col = "aquamarine", border = "aquamarine")
@@ -196,13 +193,11 @@ kpAddBaseNumbers(kp, tick.dist = 10000000, tick.len = 10, tick.col="red", cex=1,
 kpText(kp, chr="N10", x=x_N10, y=y_N10, labels=genes_N10, col = c("black", "black"))
 kpText(kp, chr="Ab10", x=x_Ab10, y=y_Ab10, labels=genes_Ab10, col = c("black", "black"))
 kpHeatmap(kp, data=Ab10_GFF_range, chr="Ab10", r0=-.8, r1 = -.7 )
-kpAddLabels(kp, labels="Genes", r0=-.8, r1 = -.7)
 kpHeatmap(kp, data=B73_GFF_range, chr="N10", r0=.4, r1 =.5 )
-kpAddLabels(kp, labels="Genes", r0=.4, r1 =.5)
 kp <- kpBars(kp, chr="Ab10", x0=xs, x1=xe, y0=yb, y1=yt, r0=-.9, r1 =-.8 )
-kpAddLabels(kp, labels="N10", r0=-.88, r1 =-.88)
-kpArrows(kp, chr="N10", x0=150834905, x1=150834905, y0=0.15, y1=-0.1, col="black")
-kpArrows(kp, chr="Ab10", x0=161788577, x1=161788577, y0=-0.4, y1=-0.22, col="black")
-kpArrows(kp, chr="N10", x0=146758676, x1=146758676, y0=0.15, y1=-0.1, col="black")
-kpArrows(kp, chr="Ab10", x0=167105020, x1=167105020, y0=-0.4, y1=-0.1, col="black")
+kpArrows(kp, chr="N10", x0=150834905, x1=150834905, y0=0.15, y1=-0.1, col="black", lwd=2)
+kpArrows(kp, chr="Ab10", x0=161788577, x1=161788577, y0=-0.4, y1=-0.22, col="black", lwd=2)
+kpArrows(kp, chr="N10", x0=146758676, x1=146758676, y0=0.15, y1=-0.1, col="black", lwd=2)
+kpArrows(kp, chr="Ab10", x0=167105020, x1=167105020, y0=-0.4, y1=-0.1, col="black", lwd=2)
 dev.off()
+
